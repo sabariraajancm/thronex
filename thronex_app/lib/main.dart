d43460b1d69f90009516a7f25aaebf60998cb82a
Sabariraajancm <sabariraajan2001.com>
2025-12-03 17:05:35 +0530

    Updated UI components and added new widgets

--- thronex_app/lib/main.dart
+++ thronex_app/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
-
+`
 void main() {
   runApp(const ThronexApp());
 }
